efa389a4bde2f6c0476270cb2f13f52edf6edf3f
Vincent Auger <vincent.auger@gmail.com>
2022-07-27 15:33:05 -0300

    skeleton progress

--- resources/src/api/sanctum.ts
+++ resources/src/api/sanctum.ts
@@ -1,3 +1,15 @@
-import { http } from "./http";
+import { http } from './http';
 
-export const csrf = () => http.get("/sanctum/csrf-cookie");
+export const useSanctum = () => {
+    const csrf = () => http.get('/sanctum/csrf-cookie');
+
+    const login = async (email: string, password: string) => {
+        await csrf();
+
+        http.post('/login', { email, password }).then();
+    };
+
+    return {
+        login,
+    };
+};
