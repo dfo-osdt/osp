4268d0034dcf336f01670b8ee7f6e4a9031b7b73
Vincent Auger <vincent.auger@gmail.com>
2022-09-14 18:00:08 -0300

    progress

--- tests/Feature/Models/OrganizationTest.php
+++ tests/Feature/Models/OrganizationTest.php
@@ -1,7 +1,19 @@
 <?php
 
-test('example', function () {
-    $response = $this->get('/');
+use App\Models\Organization;
 
-    $response->assertStatus(200);
+test('a user can get a list of all organization', function () {
+    //$this->seed();
+
+    Organization::factory()->count(20)->create();
+
+    // unauthenticated user
+    $response = $this->getJson('/api/organizations')->assertUnauthorized();
+
+    $user = \App\Models\User::factory()->create();
+
+    $response = $this->actingAs($user)->getJson('/api/organizations?limit=15')->assertOk();
+
+    expect($response->json('data'))->toHaveCount(15);
+    expect($response->json('meta.total'))->toBe(20);
 });
