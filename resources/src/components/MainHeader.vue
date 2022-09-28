e292042ae21f96e1c97e9654a68131f1035d6b59
Vincent Auger <vincent.auger@gmail.com>
2022-09-28 09:51:17 -0300

    fix in idle listener

--- resources/src/components/MainHeader.vue
+++ resources/src/components/MainHeader.vue
@@ -85,7 +85,9 @@ import { useQuasar } from 'quasar';
 const authStore = useAuthStore();
 const localeStore = useLocaleStore();
 const $q = useQuasar();
-const { idle, lastActive } = useIdle(15 * 60 * 1000); // 15 minutes
+const { idle, lastActive } = useIdle(15 * 60 * 1000, {
+    listenForVisibilityChange: false,
+}); // 15 minutes
 
 // check if user is idle, notify user, and logout if no activity
 watch(
@@ -99,24 +101,25 @@ watch(
                 (Date.now() - lastActive.value) / 1000 / 60
             );
 
+            const logoutTimeout = setTimeout(() => {
+                if (idle.value) authStore.logout();
+            }, 2 * 60 * 1000);
+
             $q.notify({
                 message: `You have been inactive for ${inactiveMinutes} minutes. You will be logged out in 2 minutes.`,
                 color: 'warning',
                 timeout: 120000,
+                progress: true,
                 actions: [
                     {
                         label: 'Stay logged in',
                         color: 'white',
                         handler: () => {
-                            /** ... */
+                            clearTimeout(logoutTimeout);
                         },
                     },
                 ],
             });
-            setTimeout(() => {
-                console.log(idle.value);
-                if (idle.value) authStore.logout();
-            }, 2 * 60 * 1000);
         }
     }
 );
