c7792fd038e0175e36c1a2cf869b3674f022dbb6
Vincent Auger <vincent.auger@gmail.com>
2022-10-24 14:26:15 -0300

    bump dependencies

--- _ide_helper.php
+++ _ide_helper.php
@@ -5,7 +5,7 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel 9.35.1.
+ * Generated for Laravel 9.36.4.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
@@ -1828,6 +1828,18 @@ namespace Illuminate\Support\Facades {
         {            //Method inherited from \Illuminate\Foundation\Console\Kernel
             /** @var \App\Console\Kernel $instance */
             $instance->bootstrap();
+        }
+
+        /**
+         * Bootstrap the application without booting service providers.
+         *
+         * @return void
+         * @static
+         */
+        public static function bootstrapWithoutBootingProviders()
+        {            //Method inherited from \Illuminate\Foundation\Console\Kernel
+            /** @var \App\Console\Kernel $instance */
+            $instance->bootstrapWithoutBootingProviders();
         }
 
         /**
@@ -13828,6 +13840,7 @@ namespace Illuminate\Support\Facades {
      * @method static \Illuminate\Routing\RouteRegistrar scopeBindings()
      * @method static \Illuminate\Routing\RouteRegistrar where(array $where)
      * @method static \Illuminate\Routing\RouteRegistrar withoutMiddleware(array|string $middleware)
+     * @method static \Illuminate\Routing\RouteRegistrar withoutScopedBindings()
      *
      * @see \Illuminate\Routing\Router
      */
@@ -16430,6 +16443,20 @@ namespace Illuminate\Support\Facades {
         {
             /** @var \Illuminate\Filesystem\FilesystemAdapter $instance */
             return $instance->size($path);
+        }
+
+        /**
+         * Get the checksum for a file.
+         *
+         * @return string|false
+         *
+         * @throws UnableToProvideChecksum
+         * @static
+         */
+        public static function checksum($path, $options = [])
+        {
+            /** @var \Illuminate\Filesystem\FilesystemAdapter $instance */
+            return $instance->checksum($path, $options);
         }
 
         /**
@@ -22543,6 +22570,20 @@ namespace  {
         {
             /** @var \Illuminate\Database\Query\Builder $instance */
             return $instance->toSql();
+        }
+
+        /**
+         * Get a single expression value from the first result of a query.
+         *
+         * @param  string  $expression
+         * @param  array  $bindings
+         * @return mixed
+         * @static
+         */
+        public static function rawValue($expression, $bindings = [])
+        {
+            /** @var \Illuminate\Database\Query\Builder $instance */
+            return $instance->rawValue($expression, $bindings);
         }
 
         /**
