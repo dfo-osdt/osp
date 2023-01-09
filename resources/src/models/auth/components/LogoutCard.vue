bdc4b7495ba38028aa17cd835d2c167eb1671e51
Vincent Auger <vincent.auger@gmail.com>
2023-01-09 17:03:04 -0400

    logout card layout

--- resources/src/models/auth/components/LogoutCard.vue
+++ resources/src/models/auth/components/LogoutCard.vue
@@ -1,13 +1,16 @@
 <template>
     <q-card flat>
         <q-card-section class="q-mt-md flex justify-around">
-            <q-img src="/assets/splash_images/undraw_adventure_map_hnin.svg" />
+            <q-img
+                src="/assets/splash_images/undraw_adventure_map_hnin.svg"
+                width="50%"
+            />
         </q-card-section>
         <q-card-section class="flex-center flex">
-            <div class="text-h2 text-primary">
+            <div class="col-12 text-h2 text-primary">
                 {{ $t('common.logged-out') }}
             </div>
-            <div class="text-body2 text-grey-8 q-mt-md">
+            <div class="col-12 text-body2 text-grey-8 q-mt-md">
                 {{ $t('logout-card.text') }}
             </div>
         </q-card-section>
