f8df5be15950bfd06ffc7379d460fbdc1200d097
Vincent Auger <vincent.auger@gmail.com>
2023-01-17 16:32:46 -0400

    translate auth emails

--- resources/views/vendor/authentication-log/emails/new.blade.php
+++ resources/views/vendor/authentication-log/emails/new.blade.php
@@ -3,16 +3,16 @@
 
 @lang('Your :app account logged in from a new device.', ['app' => config('app.name')])
 
-> **@lang('Account:')** {{ $account->email }}<br/>
-> **@lang('Time:')** {{ $time->toCookieString() }}<br/>
-> **@lang('IP Address:')** {{ $ipAddress }}<br/>
-> **@lang('Browser:')** {{ $browser }}<br/>
+> **@lang('Account'):** {{ $account->email }}<br/>
+> **@lang('Time'):** {{ $time->toCookieString() }}<br/>
+> **@lang('IP Address'):** {{ $ipAddress }}<br/>
+> **@lang('Browser'):** {{ $browser }}<br/>
 @if ($location && $location['default'] === false)
-> **@lang('Location:')** {{ $location['city'] ?? __('Unknown City') }}, {{ $location['state'], __('Unknown State') }}
+> **@lang('Location'):** {{ $location['city'] ?? __('Unknown City') }}, {{ $location['state'], __('Unknown State') }}
 @endif
 
-@lang('If this was you, you can ignore this alert. If you suspect any suspicious activity on your account, please change your password.')
+@lang('If this was you, you can ignore this alert. If you suspect any suspicious activity on your account, please change your password.')<br />
 
-@lang('Regards,')<br/>
-{{ config('app.name') }}
+@lang('Regards'),<br/>
+@lang('Open Science Portal Team')
 @endcomponent
