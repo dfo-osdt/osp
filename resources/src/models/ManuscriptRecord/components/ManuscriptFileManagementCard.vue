5b394d49083e6967bb654b7520daf3ff04d05122
Vincent Auger <vincent.auger@gmail.com>
2023-05-17 13:33:47 -0300

    wip

---
+++ resources/src/models/ManuscriptRecord/components/ManuscriptFileManagementCard.vue
@@ -0,0 +1,13 @@
+<template>
+    <div></div>
+</template>
+
+<script setup lang="ts">
+import { ManuscriptRecordResource } from '../ManuscriptRecord';
+
+const props = defineProps<{
+    manuscript: ManuscriptRecordResource;
+}>();
+</script>
+
+<style scoped></style>
