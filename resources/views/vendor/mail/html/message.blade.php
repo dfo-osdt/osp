6da23b5e90a60e3a2eb410b28bef5342e63d7c2d
Vincent Auger <vincent.auger@gmail.com>
2023-06-26 16:22:44 -0300

    translate

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -25,7 +25,7 @@
 <p>This is an automated message. Please do not reply to this email. If you believe you have received this email in error, please contact the Open Science Portal team by visiting the <a href="{{config('app.frontend_url')}}">Open Science Portal</a></p>
 <p>Ceci est un message automatisé. Veuillez ne pas répondre à ce courriel. Si vous croyez avoir reçu ce courriel par erreur, veuillez contacter l'équipe du Portail de la science ouverte en visitant le <a href="{{config('app.frontend_url')}}">Portail de la science ouverte</a></p>
 <p>---</p>
-<p>© {{ date('Y') }} Open Science Portal - Portail de la science ouverte.</p>
+<p>© {{ date('Y') }} Open Science Portal - Portail Science Ouverte.</p>
 @endcomponent
 @endslot
 @endcomponent
