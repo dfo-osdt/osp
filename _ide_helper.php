144e48af3282d4af840877c457d46359b187d9a8
Vincent Auger <vincent.auger@gmail.com>
2023-08-08 08:32:08 -0300

    composer bump

--- _ide_helper.php
+++ _ide_helper.php
@@ -4,7 +4,7 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel 10.17.0.
+ * Generated for Laravel 10.17.1.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
