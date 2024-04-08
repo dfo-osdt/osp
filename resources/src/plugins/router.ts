822256b947346561b2d7aaf99e5d49e6f6b2ee98
Vincent Auger <vincent.auger@gmail.com>
2024-04-08 18:33:37 -0300

    Refactor scrollBehavior in router.ts to handle hash navigation and savedPosition (#581) (#582)

--- resources/src/plugins/router.ts
+++ resources/src/plugins/router.ts
@@ -5,11 +5,8 @@ import routes from '@/router/routes';
 
 export const Router = createRouter({
     scrollBehavior(to, from, savedPosition) {
-        // Returning the savedPosition will result in a direct jump to the saved position
         if (to.hash) {
-            // Check for a hash in the incoming route
             return new Promise((resolve) => {
-                // Wait until the next tick to ensure the page has rendered
                 setTimeout(() => {
                     const element = document.querySelector(to.hash);
                     if (element) {
@@ -18,13 +15,11 @@ export const Router = createRouter({
                             behavior: 'smooth',
                         });
                     } else {
-                        // Fallback to top of the page if the element is not found
                         resolve({ top: 0 });
                     }
-                }, 250); // Adjust delay as needed
+                }, 250);
             });
         }
-        console.log('savedPosition', savedPosition);
         if (savedPosition) {
             return new Promise((resolve) => {
                 setTimeout(() => {
