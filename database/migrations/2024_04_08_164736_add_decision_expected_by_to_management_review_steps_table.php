fee25eb8e33d5e3c66144f62e7c9d0b36d159003
Vincent Auger <vincent.auger@gmail.com>
2024-04-08 17:00:52 -0300

    Add decision_expected_by date to existing ManagementReviewStep records

--- database/migrations/2024_04_08_164736_add_decision_expected_by_to_management_review_steps_table.php
+++ database/migrations/2024_04_08_164736_add_decision_expected_by_to_management_review_steps_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\ManagementReviewStep;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,5 +15,10 @@ return new class extends Migration
         Schema::table('management_review_steps', function (Blueprint $table) {
             $table->datetime('decision_expected_by')->nullable()->after('decision');
         });
+
+        // Update the existing records to have a decision_expected_by date
+        ManagementReviewStep::whereNull('decision_expected_by')->update([
+            'decision_expected_by' => now()->addBusinessDays(config('management_review.default_days_to_complete')),
+        ]);
     }
 };
