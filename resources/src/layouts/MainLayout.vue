bd7d0f3d675e19617849351361703cd38bad6e01
Vincent Auger <vincent.auger@dfo-mpo.gc.ca>
2024-08-01 16:50:28 -0300

    update to antfu eslint config and eslint 9

--- resources/src/layouts/MainLayout.vue
+++ resources/src/layouts/MainLayout.vue
@@ -1,74 +1,75 @@
-<template>
-    <q-layout view="hHh Lpr lff">
-        <OhDearStatusMonitor />
-        <MainHeader @toggle-left-drawer="toggleLeftDrawer"></MainHeader>
-        <MainDrawer
-            v-if="authStore.isAuthenticated"
-            v-model="authStore.leftDrawerOpen"
-            show-if-above
-            bordered
-        />
-        <q-page-container>
-            <router-view />
-        </q-page-container>
-        <MainFooter />
-    </q-layout>
-</template>
-
 <script setup lang="ts">
-import MainHeader from '@/components/MainHeader.vue';
-import MainDrawer from '@/components/MainDrawer.vue';
-import MainFooter from '@/components/MainFooter.vue';
-import { useQuasar } from 'quasar';
-import OhDearStatusMonitor from '@/components/OhDearStatusMonitor.vue';
+import { useQuasar } from 'quasar'
+import MainHeader from '@/components/MainHeader.vue'
+import MainDrawer from '@/components/MainDrawer.vue'
+import MainFooter from '@/components/MainFooter.vue'
+import OhDearStatusMonitor from '@/components/OhDearStatusMonitor.vue'
 
-const { t } = useI18n();
-const router = useRouter();
-const authStore = useAuthStore();
+const { t } = useI18n()
+const router = useRouter()
+const authStore = useAuthStore()
 
 function toggleLeftDrawer() {
-    authStore.leftDrawerOpen = !authStore.leftDrawerOpen;
+  authStore.leftDrawerOpen = !authStore.leftDrawerOpen
 }
 
 // idle timer and auto logout
-const $q = useQuasar();
+const $q = useQuasar()
 const { idle, lastActive } = useIdle(authStore.idleTimerMin * 60 * 1000, {
-    listenForVisibilityChange: false,
-}); // duration set in .env file as VITE_IDLE_TIMER_MIN
+  listenForVisibilityChange: false,
+}) // duration set in .env file as VITE_IDLE_TIMER_MIN
 
 // check if user is idle, notify user, and logout if no activity
 watch(
-    () => idle.value,
-    (isIdle) => {
-        if (isIdle) {
-            // do nothing if user is not logged in?
-            if (!authStore.isAuthenticated) return;
+  () => idle.value,
+  (isIdle) => {
+    if (isIdle) {
+      // do nothing if user is not logged in?
+      if (!authStore.isAuthenticated)
+        return
 
-            const inactiveMinutes = Math.round(
-                (Date.now() - lastActive.value) / 1000 / 60
-            );
+      const inactiveMinutes = Math.round(
+        (Date.now() - lastActive.value) / 1000 / 60,
+      )
 
-            const logoutTimeout = setTimeout(() => {
-                if (idle.value)
-                    router.push({ name: 'logout', query: { inactive: '1' } });
-            }, 2 * 60 * 1000);
+      const logoutTimeout = setTimeout(() => {
+        if (idle.value)
+          router.push({ name: 'logout', query: { inactive: '1' } })
+      }, 2 * 60 * 1000)
 
-            $q.notify({
-                message: t('common.inactive-msg', [inactiveMinutes]),
-                color: 'warning',
-                timeout: 120000,
-                progress: true,
-                actions: [
-                    {
-                        label: t('common.stay-logged-in'),
-                        color: 'white',
-                        handler: () => {
-                            clearTimeout(logoutTimeout);
-                        },
-                    },
-                ],
-            });
-        }
+      $q.notify({
+        message: t('common.inactive-msg', [inactiveMinutes]),
+        color: 'warning',
+        timeout: 120000,
+        progress: true,
+        actions: [
+          {
+            label: t('common.stay-logged-in'),
+            color: 'white',
+            handler: () => {
+              clearTimeout(logoutTimeout)
+            },
+          },
+        ],
+      })
     }
-);
+  },
+)
 </script>
+
+<template>
+  <q-layout view="hHh Lpr lff">
+    <OhDearStatusMonitor />
+    <MainHeader @toggle-left-drawer="toggleLeftDrawer" />
+    <MainDrawer
+      v-if="authStore.isAuthenticated"
+      v-model="authStore.leftDrawerOpen"
+      show-if-above
+      bordered
+    />
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+    <MainFooter />
+  </q-layout>
+</template>
