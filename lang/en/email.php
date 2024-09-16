84662f356069b37f3e29d2db294a19f9d7541f5b
MathewE <mathew.emshey@dfo-mpo.gc.ca>
2024-09-16 13:52:19 -0230

    fix(email): corrected spelling in EN email message

--- lang/en/email.php
+++ lang/en/email.php
@@ -3,9 +3,9 @@ return [
     "auth" => [
         "invitation" => [
             "expiry-note" => "The link above will expire in :days days. If the link has expired, you can simply register to access your account.",
-            "p1" => "**:name** (:email) has invited your to the Open Science Portal to review a manuscript record.",
+            "p1" => "**:name** (:email) has invited you to the Open Science Portal to review a manuscript record.",
             "p2" => "Please click the button below to verify your email address and activate your account.",
-            "p3" => "Use the teporary password below to login. You will be asked to change your password on your first login.",
+            "p3" => "Use the temporary password below to login. You will be asked to change your password on your first login.",
             "title" => "[Action Required] Your Open Science Portal account is ready"
         ],
         "reset" => [
