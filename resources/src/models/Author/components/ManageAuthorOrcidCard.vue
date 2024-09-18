4e1597b2695e5c56b1d7e8968e0cdda5b125781a
Vincent Auger <vincent.auger@dfo-mpo.gc.ca>
2024-09-18 16:17:05 -0300

    refactor(author): update ManageAuthorOrcidCard.vue
    This commit refactors the ManageAuthorOrcidCard.vue component by updating the code to use the new i18n module. It replaces the usage of the deprecated $t function with the new t function from the useI18n hook. This ensures that the component is using the latest i18n functionality and improves code maintainability.

--- resources/src/models/Author/components/ManageAuthorOrcidCard.vue
+++ resources/src/models/Author/components/ManageAuthorOrcidCard.vue
@@ -4,6 +4,7 @@ import OrcidAvatar from '@/components/OrcidAvatar.vue'
 
 const localeStore = useLocaleStore()
 const authStore = useAuthStore()
+const { t } = useI18n()
 
 const url = computed(() => {
   return `/api/user/orcid/verify?locale=${localeStore.locale}`
@@ -23,24 +24,24 @@ async function revokeToken() {
     <template #title>
       <q-icon name="mdi-plus-network-outline" size="md" />
       <span class="q-pl-sm">{{
-        $t('manage-author-profile-view.authorize-orcid')
+        t('manage-author-profile-view.authorize-orcid')
       }}</span>
     </template>
     <q-card-section v-if="isVerified">
       <p>
-        {{ $t('orcid.already-verified-text') }}
+        {{ t('orcid.already-verified-text') }}
       </p>
       <div class="text-subtitle1 q-mb-md">
         <span class="text-primary text-weight-bold">ORCID: </span>
         <a
           target="_blank"
           class="text-primary"
-          :href="authStore.user.author.data.orcid"
-        >{{ authStore.user.author.data.orcid }}</a>
+          :href="authStore.user?.author.data.orcid"
+        >{{ authStore.user?.author.data.orcid }}</a>
       </div>
       <div class="flex justify-end">
         <q-btn outline color="primary" @click="revokeToken()">
-          <OrcidAvatar size="md" /><span class="q-ml-md text-grey-8">{{ $t('ocrid.revoke-btn-text') }}
+          <OrcidAvatar size="md" /><span class="q-ml-md text-grey-8">{{ t('ocrid.revoke-btn-text') }}
           </span>
         </q-btn>
       </div>
@@ -48,13 +49,13 @@ async function revokeToken() {
     <q-card-section v-else>
       <div class="column flex flex-center">
         <div class="q-ma-md text-h5 text-grey-8">
-          {{ $t('orcid.verify-header-text') }}
+          {{ t('orcid.verify-header-text') }}
         </div>
         <div>
           <q-btn outline color="primary" :href="url">
             <OrcidAvatar size="lg" /><span
               class="q-ml-md q-my-sm text-grey-8"
-            >{{ $t('ocrid.verify-btn-text') }}
+            >{{ t('ocrid.verify-btn-text') }}
             </span>
           </q-btn>
         </div>
