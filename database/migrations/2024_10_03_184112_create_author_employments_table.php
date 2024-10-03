5c86455ce2235b10ac17533cf043d83c54c67247
Vincent Auger <vincent.auger@dfo-mpo.gc.ca>
2024-10-03 16:25:50 -0300

    progress

---
+++ database/migrations/2024_10_03_184112_create_author_employments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('author_employments', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('orcid_putcode')->nullable();
+            $table->timestamp('orcid_updated_at')->nullable();
+            $table->foreignId('author_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('organization_id')->constrained()->cascadeOnDelete();
+            $table->string('role_title')->nullable();
+            $table->string('department_name')->nullable();
+            $table->date('start_date');
+            $table->date('end_date')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('author_employments');
+    }
+};
