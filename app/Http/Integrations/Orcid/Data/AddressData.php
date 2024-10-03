889f7c5639463ccdc4583a31b5cc778c6d60180d
Vincent Auger <vincent.auger@dfo-mpo.gc.ca>
2024-10-03 16:25:50 -0300

    progress

--- app/Http/Integrations/Orcid/Data/AddressData.php
+++ app/Http/Integrations/Orcid/Data/AddressData.php
@@ -3,12 +3,13 @@
 namespace App\Http\Integrations\Orcid\Data;
 
 use Spatie\LaravelData\Data;
+use Spatie\LaravelData\Optional;
 
 class AddressData extends Data
 {
     public function __construct(
         public string $city,
-        public string $region,
+        public string|Optional $region,
         public string $country
     ) {}
 }
