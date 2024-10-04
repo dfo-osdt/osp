139673624dfb471915960aec151838e1e9e28af4
Vincent Auger <vincent.auger@dfo-mpo.gc.ca>
2024-10-04 15:33:14 -0300

    progress

--- app/Http/Integrations/Orcid/Data/DateData.php
+++ app/Http/Integrations/Orcid/Data/DateData.php
@@ -9,12 +9,27 @@ class DateData extends Data
 {
     public function __construct(
         #[MapInputName('year.value')]
-        public string $year,
+        public ?string $year,
 
         #[MapInputName('month.value')]
-        public string $month,
+        public ?string $month,
 
         #[MapInputName('day.value')]
-        public string $day
+        public ?string $day
     ) {}
+
+    /**
+     * @param  string  $date  Example: 2021-01-01
+     * @return static
+     */
+    public static function fromString(string $date): self
+    {
+        $dateParts = explode('-', $date);
+
+        return new self(
+            year: $dateParts[0] ?? null,
+            month: $dateParts[1] ?? null,
+            day: $dateParts[2] ?? null
+        );
+    }
 }
