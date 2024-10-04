d1db60aeac8f3f06ccceb01ac365efe9367779e8
Vincent Auger <vincent.auger@dfo-mpo.gc.ca>
2024-10-04 15:33:14 -0300

    progress

--- app/Http/Integrations/Orcid/Data/EmploymentData.php
+++ app/Http/Integrations/Orcid/Data/EmploymentData.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Integrations\Orcid\Data;
 
+use App\Models\AuthorEmployment;
 use Spatie\LaravelData\Attributes\MapName;
 use Spatie\LaravelData\Data;
 use Spatie\LaravelData\Optional;
@@ -9,24 +10,50 @@ use Spatie\LaravelData\Optional;
 class EmploymentData extends Data
 {
     public function __construct(
+
+        #[MapName('created-date')]
+        public TimestampData|Optional $createdDate,
+
+        #[MapName('last-modified-date')]
+        public TimestampData|Optional $lastModifiedDate,
+
+        #[MapName('put-code')]
+        public int|Optional $putCode,
+
         #[MapName('department-name')]
-        public string|Optional $departmentName,
+        public string|null|Optional $departmentName,
 
         #[MapName('role-title')]
-        public string|Optional $roleTitle,
+        public string|null|Optional $roleTitle,
 
         #[MapName('start-date')]
-        public DateData|Optional $startDate,
+        public DateData|null|Optional $startDate,
 
         #[MapName('end-date')]
-        public DateData|Optional $endDate,
+        public DateData|null|Optional $endDate,
 
         public OrganizationData $organization,
 
-        public UrlData|Optional $url,
+        public UrlData|null|Optional $url,
 
         #[MapName('external-ids')]
-        public ExternalIdsData $externalIds
+        public ?ExternalIdsData $externalIds
 
     ) {}
+
+    public static function fromModel(AuthorEmployment $authorEmployment)
+    {
+        return new self(
+            createdDate: Optional::create(),
+            lastModifiedDate: Optional::create(),
+            putCode: $authorEmployment->orcid_putcode ? $authorEmployment->orcid_putcode : Optional::create(),
+            departmentName: $authorEmployment->department_name,
+            roleTitle: $authorEmployment->role_title,
+            startDate: $authorEmployment->start_date ? DateData::from($authorEmployment->start_date->format('Y-m-d')) : null,
+            endDate: $authorEmployment->end_date ? DateData::from($authorEmployment->end_date->format('Y-m-d')) : null,
+            organization: OrganizationData::fromModel($authorEmployment->organization),
+            url: Optional::create(),
+            externalIds: null
+        );
+    }
 }
--- app/Models/AuthorEmployment.php
+++ app/Models/AuthorEmployment.php
@@ -9,4 +9,28 @@ class AuthorEmployment extends Model
 {
     /** @use HasFactory<\Database\Factories\AuthorEmploymentFactory> */
     use HasFactory;
+
+    protected function casts(): array
+    {
+        return [
+            'start_date' => 'date',
+            'end_date' => 'date',
+            'orcid_updated_at' => 'timestamp',
+        ];
+    }
+
+    public function author()
+    {
+        return $this->belongsTo(Author::class);
+    }
+
+    /**
+     * Note that we allow a different organization here instead
+     * of the one in the author's profile as the author may
+     * have had been employed by a different organization
+     */
+    public function organization()
+    {
+        return $this->belongsTo(Organization::class);
+    }
 }
