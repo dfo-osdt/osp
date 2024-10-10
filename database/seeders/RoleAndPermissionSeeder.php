bb98e8ac56a2f03ab506167204ab72d4bb21251e
MathewE <mathew.emshey@dfo-mpo.gc.ca>
2024-10-10 10:50:19 -0230

    chore

--- database/seeders/RoleAndPermissionSeeder.php
+++ database/seeders/RoleAndPermissionSeeder.php
@@ -47,6 +47,8 @@ class RoleAndPermissionSeeder extends Seeder
             Permission::findOrCreate('view_telescope'),
             Permission::findOrCreate('view_horizon'),
             Permission::findOrCreate('view_pulse'),
+	    Permission::findOrCreate('view_any_user'),
+	    Permission::findOrCreate('update_any_user'),
         ]);
 
         $adminRole = Role::findOrCreate('admin');
