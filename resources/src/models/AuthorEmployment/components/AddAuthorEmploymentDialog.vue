ef4cac87ea8392c14fa49c445202a18a8ec8c180
Vincent Auger <vincent.auger@dfo-mpo.gc.ca>
2024-10-31 10:41:30 -0300

    wip: front end employment management

---
+++ resources/src/models/AuthorEmployment/components/AddAuthorEmploymentDialog.vue
@@ -0,0 +1,111 @@
+<script setup lang="ts">
+import BaseDialog from '@/components/BaseDialog.vue'
+import DateInput from '@/components/DateInput.vue'
+import OrganizationSelect from '@/models/Organization/components/OrganizationSelect.vue'
+import { useQuasar } from 'quasar'
+import { AuthorEmploymentService } from '../AuthorEmployement'
+
+const props = defineProps<{
+  authorId: number
+}>()
+
+const emit = defineEmits<{
+  changed: []
+  deleted: []
+}>()
+
+// form data
+const organizationId = 1
+const roleTitle = ref<string | null>(null)
+const departmentName = ref<string | null>(null)
+const startDate = ref<string | null>(null)
+const endDate = ref<string | null>(null)
+
+const { t } = useI18n()
+const q = useQuasar()
+
+const authorEmploymentService = new AuthorEmploymentService(props.authorId)
+
+async function createAuthorEmployment() {
+  q.dialog({
+    title: t('orcid-employment-edit.update-confirmation-title'),
+    message: t('orcid-employment-edit.update-confirmation-message'),
+    ok: {
+      label: t('common.confirm'),
+      color: 'primary',
+    },
+    cancel: true,
+  }).onOk(async () => {
+    await authorEmploymentService.create({
+      author_id: props.authorId,
+      organization_id: organizationId,
+      role_title: roleTitle.value,
+      department_name: departmentName.value,
+      start_date: startDate.value as string,
+      end_date: endDate.value,
+    })
+    emit('changed')
+  })
+}
+</script>
+
+<template>
+  <BaseDialog :title="t('orcid-employment-add.dialog-title')">
+    <q-card-section>
+      {{ t('orcid-employment-edit.dialog-description') }}
+    </q-card-section>
+    <q-form @submit="createAuthorEmployment">
+      <q-card-section>
+        <OrganizationSelect v-model="organizationId" label="Organization" outlined disable hide-hint class="q-mb-md" />
+        <q-input
+          v-model="roleTitle"
+          :label="t('orcid-employment-edit.role-title')"
+          outlined
+          :rules="[(val) => !!val || t('common.required'),
+                   (val) => val.length <= 50 || t('common.validation.must-be-less-than-x-characters', [50]),
+          ]"
+          class="q-mb-md"
+        />
+        <q-input
+          v-model="departmentName"
+          :label="t('orcid-employment-edit.department-name')"
+          outlined
+          :rules="[(val) => !!val || t('common.required'),
+                   (val) => val.length <= 50 || t('common.validation.must-be-less-than-x-characters', [50]),
+          ]"
+          class="q-mb-md"
+          :hint="t('orcid-employment-edit.department-name-hint')"
+          :hide-hint="false"
+        />
+        <DateInput
+          v-model="startDate"
+          label="Start Date"
+          :max-date="endDate"
+          outlined
+          :rules="[(val : string) => !!val || t('common.required')]"
+          class="q-mb-md"
+        />
+        <DateInput
+          v-model="endDate"
+          label="End Date"
+          :min-date="startDate"
+          outlined
+          clearable
+          class="q-mb-md"
+        />
+      </q-card-section>
+      <q-card-actions class="justify-end">
+        <q-btn v-close-popup :label="$t('common.cancel')" color="primary" outline />
+        <q-btn
+          type="submit"
+          color="primary"
+          :label="$t('common.create')"
+        />
+      </q-card-actions>
+    </q-form>
+  </BaseDialog>
+</template>
+
+<style scoped>
+
+</style>
