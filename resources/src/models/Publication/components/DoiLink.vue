30f9e6ea50bbea189b593b2576292a9c9fa7d0b3
Vincent Auger <vincent.auger@dfo-mpo.gc.ca>
2024-11-25 16:24:37 -0400

    fix: update DoiLink component to use doi directly as href

--- resources/src/models/Publication/components/DoiLink.vue
+++ resources/src/models/Publication/components/DoiLink.vue
@@ -2,12 +2,10 @@
 const props = defineProps<{
   doi: string | null
 }>()
-
-const url = computed(() => `https://doi.org/${props.doi}`)
 </script>
 
 <template>
-  <a v-if="doi" :href="url" target="_blank">{{ doi }}</a>
+  <a v-if="doi" :href="doi" target="_blank">{{ doi }}</a>
   <span v-else> - </span>
 </template>
 
