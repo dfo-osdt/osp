2a16a1adbc4e90131b4a5ae1d7f9101071a44a77
Vincent Auger <vincent.auger@dfo-mpo.gc.ca>
2024-11-28 13:33:38 -0400

    fix: orcid unauthenticated brand guidelines

--- resources/src/components/OrcidIcon.vue
+++ resources/src/components/OrcidIcon.vue
@@ -1,8 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { unauthenticated = false } = defineProps<{
+  unauthenticated?: boolean
+}>()
+
+const svgLink = computed(() => {
+  return unauthenticated
+    ? 'img:/assets/orcid/orcid.logo.unauth.svg'
+    : 'img:/assets/orcid/orcid.logo.icon.svg'
+})
+</script>
 
 <template>
   <q-icon
-    name="img:/assets/orcid/orcid.logo.icon.svg"
+    :name="svgLink"
     size="16px"
     alt="ORCID iD logo"
   />
