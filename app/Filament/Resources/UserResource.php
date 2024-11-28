63fb4a764b4cc21a43278fa401419633260c758a
MathewE <mathew.emshey@dfo-mpo.gc.ca>
2024-11-28 15:58:13 -0330

    feat(librarium): added verify email button

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -26,17 +26,31 @@ class UserResource extends Resource
                     ->Filled()
                     ->required(),
                 Forms\Components\TextInput::make('last_name')
-                    ->disabledOn('edit')
-                    ->Filled(),
-                Forms\Components\Section::make([
+					  ->disabledOn('edit')
+					  ->Filled(),
+		
+                Forms\Components\Section::make('User Information')
+		->schema([
                     Forms\Components\TextInput::make('email')
                         ->Filled()
                         ->rules(['bail', 'required', 'string', 'email', new AuthorizedEmailDomain]),
                     Forms\Components\CheckboxList::make('roles')
 						 ->relationship(titleAttribute: 'name')
 						 ->label('Roles'),
+		]),
+		
+		Forms\Components\Section::make('Available Actions')
+					->description('Actions are instantaneous and may disappear once applied!')
+		->schema([
 		    Forms\Components\Toggle::make('active')
-					   ->dehydrated(false),
+		    ->label('Activate User')
+					   ->dehydrated(false)
+		    ->onColor('success'),
+		    Forms\Components\Actions::make([
+			Forms\Components\Actions\Action::make('verify_email')
+						       ->hidden(fn ($record) => $record && $record->email_verified_at)
+						       ->action('setVerifiedEmail'),
+		    ]),
                 ]),
             ]);
     }
@@ -54,62 +68,65 @@ class UserResource extends Resource
                 Tables\Columns\TextColumn::make('email')
                     ->searchable(),
                 Tables\Columns\IconColumn::make('email_verified_at')
-                    ->boolean()
-                    ->sortable(),
-                Tables\Columns\IconColumn::make('active')
-                    ->boolean()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('roles.name')
-                    ->badge()
-                    ->color(fn (string $state): string => match ($state) {
-                        'author' => 'success',
-                        'director' => 'warning',
-                        'admin' => 'danger',
-                        'default' => 'info',
-                    })
-                    ->searchable(),
-            ])
-            ->filters([
-                Tables\Filters\TernaryFilter::make('email_verified_at')
-                    ->label('Verified')
-                    ->nullable()
-                    ->placeholder('All'),
-                Tables\Filters\SelectFilter::make('active')
-                    ->options([
-                        true => 'Active',
-                        false => 'Inactive',
-                    ]),
-                Tables\Filters\Filter::make('no_roles')
-                    ->label('No Roles')
-                    ->query(fn ($query) => $query->whereDoesntHave('roles')),
-                Tables\Filters\SelectFilter::make('roles')
-                    ->relationship('roles', 'name')
-                    ->label('Role'),
-            ])
-            ->actions([
-                Tables\Actions\EditAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    // Placeholder
-                ]),
-            ])
-            ->defaultSort('first_name');
+		->label('Email Verified')
+					 ->default('heroicon-o-x-circle')
+					 ->icon(fn ($state) => $state instanceof \DateTime ? 'heroicon-o-check-circle' : 'heroicon-o-x-circle')
+					 ->color(fn ($state) => $state instanceof \DateTime  ? 'success' : 'danger')
+					 ->sortable(),
+		Tables\Columns\IconColumn::make('active')
+					 ->boolean()
+					 ->sortable(),
+		Tables\Columns\TextColumn::make('roles.name')
+					 ->badge()
+					 ->color(fn (string $state): string => match ($state) {
+					     'author' => 'success',
+					     'director' => 'warning',
+					     'admin' => 'danger',
+					     'default' => 'info',
+					 })
+					 ->searchable(),
+	    ])
+	    ->filters([
+		Tables\Filters\TernaryFilter::make('email_verified_at')
+					    ->label('Verified')
+					    ->nullable()
+					    ->placeholder('All'),
+		Tables\Filters\SelectFilter::make('active')
+					   ->options([
+					       true => 'Active',
+					       false => 'Inactive',
+					   ]),
+		Tables\Filters\Filter::make('no_roles')
+				     ->label('No Roles')
+				     ->query(fn ($query) => $query->whereDoesntHave('roles')),
+		Tables\Filters\SelectFilter::make('roles')
+					   ->relationship('roles','name')
+					   ->label('Role')
+	    ])
+	    ->actions([
+		Tables\Actions\EditAction::make(),
+	    ])
+	    ->bulkActions([
+		Tables\Actions\BulkActionGroup::make([
+		    // Placeholder
+		]),
+	    ])
+	    ->defaultSort('first_name');
     }
 
     public static function getRelations(): array
     {
-        return [
-            //
-        ];
+	return [
+	    //
+	];
     }
 
     public static function getPages(): array
     {
-        return [
-            'index' => Pages\ListUsers::route('/'),
-            'create' => Pages\CreateUser::route('/create'),
-            'edit' => Pages\EditUser::route('/{record}/edit'),
-        ];
+	return [
+	    'index' => Pages\ListUsers::route('/'),
+	    'create' => Pages\CreateUser::route('/create'),
+	    'edit' => Pages\EditUser::route('/{record}/edit'),
+	];
     }
 }
