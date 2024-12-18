0e1769c5e31e354a2cedf54f4a21b60659fea21e
MathewEm <mathew.emshey@dfo-mpo.gc.ca>
2024-12-18 11:44:43 -0330

    update

--- tests/Feature/Filament/UserResourceTest.php
+++ tests/Feature/Filament/UserResourceTest.php
@@ -2,61 +2,136 @@
 
 namespace Tests\Feature\Filament;
 
+use App\Filament\Resources\UserResource\Pages\CreateUser;
+use App\Filament\Resources\UserResource\Pages\EditUser;
+use App\Filament\Resources\UserResource\Pages\ListUsers;
 use App\Models\User;
 use Tests\TestCase;
-use Livewire\livewire;
+
+use function Pest\Livewire\livewire;
 
 
 
-test('An unauthorized user cannot access the librarium pages', function ($path) {
+test('An unauthorized user cannot render the index page', function () {
     $user = User::factory()->create();
 
-    $response = $this->actingAs($user)->get($path);
-    $response->assertForbidden();
-})->with([
-    '/librarium',
-    '/librarium/users'
-]);
+    $this->actingAs($user)->get(ListUsers::getUrl())->assertForbidden();
+});
 
-test('An admin can access the librarium pages', function (string $path) {
+test('An unauthorized user cannot render the create page', function () {
+    $user = User::factory()->create();
+
+    $this->actingAs($user)->get(CreateUser::getUrl())->assertForbidden();
+});
+
+test('An unauthorized user cannot render the edit page', function () {
+    $user = User::factory()->create();
+    $record = User::factory()->create();
+
+    $this->actingAs($user)->livewire(EditUser::class, ['record' => $record->getRouteKey()])->assertForbidden();
+});
+
+test('An admin can render the index page', function () {
     $admin = User::factory()->create();
     $admin->assignRole('admin');
 
-    $response = $this->actingAs($admin)->get($path);
-    $response->assertOk();
+    $this->actingAs($admin)->get(ListUsers::getUrl())->assertOk();
+});
+
+test('An admin cannot render the user create page', function () {
+    $admin = User::factory()->create();
+    $admin-> assignRole('admin');
+
+    $this->actingAs($admin)->get(CreateUser::getUrl())->assertForbidden();
+});
+
+test('User Table has column', function (string $column) {
+    $admin = User::factory()->create();
+    $admin-> assignRole('admin');
+    
+    $this->actingAs($admin)->livewire(ListUsers::class)->assertTableColumnExists($column);
 })->with([
-    '/librarium',
-    '/librarium/users'
+    'first_name',
+    'last_name',
+    'email',
+    'email_verified_at',
+    'active',
+    'roles.name'
 ]);
 
-test('An admin cannot access user create page', function () {
+test('Admin can render the User Table column', function ($column) {
     $admin = User::factory()->create();
     $admin-> assignRole('admin');
+    
+    $this->actingAs($admin)->livewire(ListUsers::class)->assertCanRenderTableColumn($column);
+})->with([
+    'first_name',
+    'last_name',
+    'email',
+    'email_verified_at',
+    'active',
+    'roles.name'
+]);
 
-    $response = $this->actingAs($admin)->get('/librarium/users/create');
-    $response->assertForbidden();
-});
-
-test('An admin cannot call create method', function () {
+test('Admin can sort the User Table column', function ($column) {
     $admin = User::factory()->create();
     $admin-> assignRole('admin');
+    $records = User::factory(5)->create();
+    
+    $this->actingAs($admin)->livewire(ListUsers::class)
+	 ->sortTable($column)
+	 ->assertCanSeeTableRecords($records->sortBy($column), inOrder: true)
+	 ->sortTable($column, 'desc')
+	 ->assertCanSeeTableRecords($records->sortByDesc($column), inOrder: true);
+})->with([
+    'first_name',
+    'last_name',
+    'email_verified_at',
+    'active',
+]);
 
-    $user = User::factory()->create();
+test('Admin can search the User Table column', function (string $column) {
+    $admin = User::factory()->create();
+    $admin-> assignRole('admin');
+    $records = User::factory(5)->create();
 
-    $response = $this->actingAs($admin)->delete(route('filament.librarium.resources.users.create', $user));
-    $response->assertStatus(405);
-});
+    $value = $records->first()->{$column};
+    
+    $this->actingAs($admin)->livewire(ListUsers::class)
+	 ->searchTable($value)
+	 ->assertCanSeeTableRecords($records->where($column, $value))
+	 ->assertCanNotSeeTableRecords($records->where($column, '!=', $value));
+})->with([
+    'first_name',
+    'last_name',
+    'email',
+    'roles.name',
+]);
 
 test('An admin can edit an existing user', function () {
     $admin = User::factory()->create();
     $admin-> assignRole('admin');
 
-    $user = User::factory()->create();
+    $record = User::factory()->create();
+    $newRecord = User::factory()->make();
 
-    livewire(UserResouce\Pages\EditUser::class, [
-	'user' => $user->getRouteKey(),
-    ])->assertFormSet([
-	'id' => $user->id,
-	'first_name' => $user->first_name,
-	]);
+    $this->actingAs($admin)->livewire(Edituser::class, ['record' => $record->getRouteKey()])
+	 ->fillForm([
+	     'email' => $newRecord->email,
+	 ])
+	 ->assertActionExists('save')
+	 ->call('save')
+   	 ->assertHasNoFormErrors();
+
+    $this->actingAs($admin)->assertDatabaseHas(User::class, [
+	'email' => $newRecord->email,
+    ]);
+});
+
+test('An admin cannot render a delete button', function () {
+    $admin = User::factory()->create();
+    $admin-> assignRole('admin');
+
+    $this->actingAs($admin)->get(ListUsers::getUrl())
+    ->assertDontSee('Delete');
 });
