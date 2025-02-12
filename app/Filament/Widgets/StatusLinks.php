a657ea0b7bfb21c3075eabc6c6dd3940469e84ae
MathewEm <mathew.emshey@dfo-mpo.gc.ca>
2025-02-12 12:22:48 -0330

    chore(librarium): fixed formatting for pint test

--- app/Filament/Widgets/StatusLinks.php
+++ app/Filament/Widgets/StatusLinks.php
@@ -7,19 +7,18 @@ use Filament\Widgets\StatsOverviewWidget\Stat;
 
 class StatusLinks extends BaseWidget
 {
-
     protected function getStats(): array
     {
         return [
-            Stat::make('','System Health Panel')
+            Stat::make('', 'System Health Panel')
                 ->url(url('/health'))
                 ->extraAttributes(['class' => 'text-center']),
 
-            Stat::make("", 'Horizon')
+            Stat::make('', 'Horizon')
                 ->url(url('/horizon'))
                 ->extraAttributes(['class' => 'text-center']),
 
-                Stat::make('', 'Pulse')
+            Stat::make('', 'Pulse')
                 ->url(url('/pulse'))
                 ->extraAttributes(['class' => 'text-center']),
         ];
--- app/Providers/Filament/LibrariumPanelProvider.php
+++ app/Providers/Filament/LibrariumPanelProvider.php
@@ -39,7 +39,6 @@ class LibrariumPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
