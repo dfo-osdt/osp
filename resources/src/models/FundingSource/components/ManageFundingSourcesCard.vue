3dc18fa4d82334cb96223c55c70af27b070950eb
Vincent Auger <vincent.auger@dfo-mpo.gc.ca>
2025-08-29 15:27:38 -0300

    ci: linting tests

--- resources/src/models/FundingSource/components/ManageFundingSourcesCard.vue
+++ resources/src/models/FundingSource/components/ManageFundingSourcesCard.vue
@@ -1,66 +1,67 @@
 <script setup lang="ts">
-import type { FundableType, FundingSourceResource } from '../FundingSource';
-import { useQuasar } from 'quasar';
-import ContentCard from '@/components/ContentCard.vue';
-import FormSectionStatusIcon from '@/components/FormSectionStatusIcon.vue';
-import { FundingSourceService } from '../FundingSource';
-import CreateFundingSourceDialog from './CreateFundingSourceDialog.vue';
-import FundingSourceList from './FundingSourceList.vue';
+import type { FundableType, FundingSourceResource } from '../FundingSource'
+import { useQuasar } from 'quasar'
+import ContentCard from '@/components/ContentCard.vue'
+import FormSectionStatusIcon from '@/components/FormSectionStatusIcon.vue'
+import { FundingSourceService } from '../FundingSource'
+import CreateFundingSourceDialog from './CreateFundingSourceDialog.vue'
+import FundingSourceList from './FundingSourceList.vue'
 
 const props = defineProps<{
-  fundableId: number;
-  fundableType: FundableType;
-  readonly?: boolean;
-}>();
-const $q = useQuasar();
-const { t } = useI18n();
+  fundableId: number
+  fundableType: FundableType
+  readonly?: boolean
+}>()
+const $q = useQuasar()
+const { t } = useI18n()
 
-const loading = ref(true);
-const fundingSources = ref<FundingSourceResource[]>([]);
-const showCreateDialog = ref(false);
+const loading = ref(true)
+const fundingSources = ref<FundingSourceResource[]>([])
+const showCreateDialog = ref(false)
 
 onMounted(async () => {
-  await loadFundingSources();
-});
+  await loadFundingSources()
+})
 
 async function loadFundingSources() {
-  loading.value = true;
-  const fundingService = new FundingSourceService(props.fundableType);
-  const response = await fundingService.all(props.fundableId);
-  fundingSources.value = response.data;
-  loading.value = false;
+  loading.value = true
+  const fundingService = new FundingSourceService(props.fundableType)
+  const response = await fundingService.all(props.fundableId)
+  fundingSources.value = response.data
+  loading.value = false
 }
 
 async function createFundingSource(fundingSource: FundingSourceResource) {
-  fundingSources.value.push(fundingSource);
+  fundingSources.value.push(fundingSource)
   $q.notify({
     message: t('common.created'),
     color: 'positive',
     icon: 'mdi-check-circle',
-  });
+  })
   // close dialog
-  showCreateDialog.value = false;
+  showCreateDialog.value = false
 }
 
 async function editFundingSource(fundingSource: FundingSourceResource) {
   const index = fundingSources.value.findIndex(
-    (fs) => fs.data.id === fundingSource.data.id,
-  );
-  fundingSources.value.splice(index, 1, fundingSource);
+    fs => fs.data.id === fundingSource.data.id,
+  )
+  fundingSources.value.splice(index, 1, fundingSource)
 }
 
 async function deleteFundingSource(fundingSource: FundingSourceResource) {
-  const fundingService = new FundingSourceService(props.fundableType);
+  const fundingService = new FundingSourceService(props.fundableType)
   try {
-    await fundingService.delete(fundingSource.data);
-    loadFundingSources();
+    await fundingService.delete(fundingSource.data)
+    loadFundingSources()
     $q.notify({
       message: t('common.deleted'),
       color: 'positive',
       icon: 'mdi-check-circle',
-    });
-  } catch (err) {
-    console.error(err);
+    })
+  }
+  catch (err) {
+    console.error(err)
   }
 }
 </script>
@@ -80,8 +81,8 @@ async function deleteFundingSource(fundingSource: FundingSourceResource) {
       :funding-sources="fundingSources"
       :fundable-type="fundableType"
       :readonly="readonly"
-      @editedFundingSource="editFundingSource"
-      @deleteFundingSource="deleteFundingSource"
+      @edited-funding-source="editFundingSource"
+      @delete-funding-source="deleteFundingSource"
     />
     <q-card-actions class="justify-end q-px-none q-pt-md">
       <q-btn
@@ -98,7 +99,7 @@ async function deleteFundingSource(fundingSource: FundingSourceResource) {
       v-model="showCreateDialog"
       :fundable-type="fundableType"
       :fundable-id="fundableId"
-      @create:funding-source="createFundingSource"
+      @create-funding-source="createFundingSource"
     />
   </ContentCard>
 </template>
