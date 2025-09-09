06bb78426e330ce4f1f3f3124d8d3f600807d228
Vincent Auger <vincent.auger@dfo-mpo.gc.ca>
2025-09-08 22:22:36 -0300

    chore: update
    eslint update created linting errors, ran with --fix

--- resources/src/router/routes.ts
+++ resources/src/router/routes.ts
@@ -113,7 +113,7 @@ const routes: RouteRecordRaw[] = [
         path: '/my-reviews',
         component: () =>
           import(
-            '@/models/ManagementReviewStep/views/MyManagementReviewStepsView.vue'
+            '@/models/ManagementReviewStep/views/MyManagementReviewStepsView.vue',
           ),
         meta: { requiresAuth: true },
       },
@@ -127,7 +127,7 @@ const routes: RouteRecordRaw[] = [
         path: '/regional-manuscripts',
         component: () =>
           import(
-            '@/models/ManuscriptRecord/views/RegionalManuscriptRecordsView.vue'
+            '@/models/ManuscriptRecord/views/RegionalManuscriptRecordsView.vue',
           ),
         meta: { requiresAuth: true },
         name: 'regional-manuscripts',
@@ -170,7 +170,7 @@ const routes: RouteRecordRaw[] = [
             path: 'form',
             component: () =>
               import(
-                '@/models/ManuscriptRecord/views/ManuscriptRecordFormView.vue'
+                '@/models/ManuscriptRecord/views/ManuscriptRecordFormView.vue',
               ),
             props: route => ({ id: Number(route.params.id) }),
             name: 'manuscript.form',
@@ -179,7 +179,7 @@ const routes: RouteRecordRaw[] = [
             path: 'reviews',
             component: () =>
               import(
-                '@/models/ManagementReviewStep/views/ManagementReviewStepsView.vue'
+                '@/models/ManagementReviewStep/views/ManagementReviewStepsView.vue',
               ),
             props: route => ({ id: Number(route.params.id) }),
             name: 'manuscript.reviews',
@@ -188,7 +188,7 @@ const routes: RouteRecordRaw[] = [
             path: 'progress',
             component: () =>
               import(
-                '@/models/ManuscriptRecord/views/ManuscriptRecordProgressView.vue'
+                '@/models/ManuscriptRecord/views/ManuscriptRecordProgressView.vue',
               ),
             props: route => ({ id: Number(route.params.id) }),
             name: 'manuscript.progress',
@@ -197,7 +197,7 @@ const routes: RouteRecordRaw[] = [
             path: 'sharing',
             component: () =>
               import(
-                '@/models/ManuscriptRecord/views/ManuscriptRecordSharingView.vue'
+                '@/models/ManuscriptRecord/views/ManuscriptRecordSharingView.vue',
               ),
             props: route => ({ id: Number(route.params.id) }),
             name: 'manuscript.sharing',
