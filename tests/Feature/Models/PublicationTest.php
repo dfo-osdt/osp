878a62cc8d957f39a16ed290a40410bc7544d8d5
Vincent Auger <vincent.auger@dfo-mpo.gc.ca>
2026-01-14 15:55:58 -0400

    refactor: user publications

--- tests/Feature/Models/PublicationTest.php
+++ tests/Feature/Models/PublicationTest.php
@@ -41,7 +41,7 @@ test('a user can get a list of publications with a filter', function (): void {
 /** Test that a user can get a list of their publications */
 test('a user can get a list of their publications', function (): void {
     $user = User::factory()->create();
-    $publications = Publication::factory()->count(5)->create(['user_id' => $user->id]);
+    $publications = Publication::factory()->withAuthors()->count(5)->create(['user_id' => $user->id]);
     Publication::factory()->count(5)->create();
 
     $response = $this->actingAs($user)->getJson('/api/my/publications');
