4144a0ea71651ef3c40c13298443d5dc13c1e876
Vincent Auger <vincent.auger@dfo-mpo.gc.ca>
2026-02-27 09:56:33 -0400

    feat: better handling of malformed Azure response
    redirects to login again

---
+++ tests/Feature/Auth/AzureOAuthCallbackTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\Azure\AzureOAuthController;
+use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+use Laravel\Socialite\Two\InvalidStateException;
+
+it('redirects to login with error when azure oauth state is invalid', function () {
+    Route::get('/oauth/azure/callback', [AzureOAuthController::class, 'callback']);
+
+    Socialite::shouldReceive('driver')
+        ->with('azure')
+        ->andReturnSelf()
+        ->shouldReceive('user')
+        ->andThrow(new InvalidStateException);
+
+    $response = $this->get('/oauth/azure/callback');
+
+    $response->assertRedirect();
+    expect($response->headers->get('Location'))
+        ->toContain('error=oauth_error')
+        ->toContain('error_description=');
+});
