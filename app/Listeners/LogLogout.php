8818f01f3509958ea8b339138547030f8e9e3712
Vincent Auger <vincent.auger@dfo-mpo.gc.ca>
2026-03-30 16:58:34 -0300

    refactor: update LogLogout

--- app/Listeners/LogLogout.php
+++ app/Listeners/LogLogout.php
@@ -3,6 +3,7 @@
 namespace App\Listeners;
 
 use Illuminate\Auth\Events\Logout;
+use Illuminate\Support\Facades\Request;
 
 class LogLogout
 {
@@ -10,6 +11,9 @@ class LogLogout
     {
         $latest = $event->user->authentications()
             ->whereNull('logout_at')
+            ->whereTrue('login_successful')
+            ->where('ip_address', Request::ip())
+            ->where('user_agent', Request::userAgent())
             ->latest('login_at')
             ->first();
 
