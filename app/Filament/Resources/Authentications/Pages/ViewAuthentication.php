aab7f39810c849996af2f2b653475d4d3cfde85d
Vincent Auger <vincent.auger@dfo-mpo.gc.ca>
2026-04-07 10:19:31 -0300

    sytle: declare strict types

--- app/Filament/Resources/Authentications/Pages/ViewAuthentication.php
+++ app/Filament/Resources/Authentications/Pages/ViewAuthentication.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Filament\Resources\Authentications\Pages;
 
 use App\Filament\Resources\Authentications\Authentications\AuthenticationResource;
--- app/Filament/Resources/Regions/Pages/EditRegion.php
+++ app/Filament/Resources/Regions/Pages/EditRegion.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Filament\Resources\Regions\Pages;
 
 use App\Filament\Resources\Regions\Regions\RegionResource;
